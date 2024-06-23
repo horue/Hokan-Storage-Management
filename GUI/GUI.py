524eda94d310bc73cfd9379bcab1a5901861faee
Jorge <jorgejapao00@gmail.com>
2024-06-22 22:49:27 -0300

    Add new product page created

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -24,9 +24,36 @@ def bar1(fm):
     o1.delete(selected_value)
     o1.grid(row=2, column=0, pady=10, padx=10)
 
+def novo_produto(f2):
+    for widget in f2.winfo_children():
+        widget.destroy()
+
+    l1 = ct.CTkLabel(f2, text='Nome do Produto')
+    l1.pack(pady=10)
+
+    e1 = ct.CTkEntry(f2)
+    e1.pack()
+
+    l2 =ct.CTkLabel(f2, text='Quantidade do Produto')
+    l2.pack(pady=10)
+
+    global s1
+    s1 = ct.IntVar()
+    s1 = CTkSpinbox(f2,
+          start_value = 1,
+          min_value = 1,
+          max_value = 99,
+          scroll_value = 1,
+          variable = s1)
+    s1.pack()
+
+
 def frame1(f1):
     b1 = ct.CTkButton(f1, text='Nova Venda')
     b1.pack(pady=10)
+
+    b4 = ct.CTkButton(f1, text='Adicionar Produto', command=lambda:novo_produto(f2))
+    b4.pack(pady=10)
     
     b2 = ct.CTkButton(f1, text='Verificar Vendas')
     b2.pack(pady=10)
@@ -58,6 +85,7 @@ def initial(root):
 
     frame1(f1)
 
+    global f2
     f2=ct.CTkScrollableFrame(fm, width=950, height=620, label_text='Menu')
     f2.grid(column=1, row=1, padx=30, pady=(30,0))
 
